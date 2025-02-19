cb879253102abbcada0267e9ab21391bf8896582
jung2012pt <jung-pt@hotmail.com>
2025-02-19 20:35:26 +0700

    first commit

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  // Enable CORS for all origins (you can configure it further for security)
-  app.enableCors();
+  
+  // Enable CORS for all origins
+  app.enableCors({
+    origin: '*', // This will allow all domains (use cautiously in production)
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type'],
+  });
 
   await app.listen(3000);
 }
